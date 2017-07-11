8ac50eb216b37226efe01532d5502872b8b037db
kseans8 <kseans8@gmail.com>
2017-07-11 12:06:15 -0400

    MVP: working randomizer using just ruby

---
+++ server.rb
@@ -0,0 +1,42 @@
+require "sinatra"
+require "json"
+require "pry"
+set :bind, '0.0.0.0'
+
+get "/" do
+  redirect to("new-question")
+end
+
+get "/new-question" do
+  question_hash = random_question
+  @question = question_hash.keys.first
+  @answer = question_hash[:answer]
+  @options = question_hash[question_hash.keys.first].shuffle
+  # binding.pry
+  erb :index
+end
+
+def random_question
+  QUESTIONS.sample
+end
+
+QUESTIONS = [
+  {
+    "What year was the United States of America founded?": [
+      "1492",
+      "1776",
+      "1792",
+      "1476"
+    ],
+    "answer": "1776"
+  },
+  {
+    "Which number is a prime number?": [
+      "4",
+      "6",
+      "9",
+      "7"
+    ],
+    "answer": "7"
+  }
+]
